1a98cfb452871cbb4721a88f20ae0f7405a0315e
Ishibashi Ryo <ishibashi@iplab.cs.tsukuba.ac.jp>
2024-08-22 17:52:50 +0900

    MediaPipeを用いた，ユーザの頭部方向の取得

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -2,11 +2,33 @@ import { LocalAudioStream, LocalDataStream, LocalP2PRoomMember, LocalStream, Loc
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { RemoteMedia } from "./RemoteMedia";
 import "./MainContent.css"
+import { FaceMesh, Results } from '@mediapipe/face_mesh';
+import { Camera } from "@mediapipe/camera_utils";
+import Webcam from "react-webcam";
 
 interface WindowInfo {
     top: number;
     left: number;
     width: number;
+}
+
+// 数値型リストの要素の平均値を求める関数
+function Average(number_list: number[]) {
+  let sum = 0;  // 引数のリスト内の要素の合計値
+  number_list.forEach((number) => {
+    sum += number;
+  })
+  return sum / number_list.length;
+}
+
+// 2つのベクトル（数値型リスト）の内積を求める関数
+function Inner(number_list1: number[], number_list2: number[]) {
+  return number_list1[0] * number_list2[0] + number_list1[1] * number_list2[1];
+}
+
+// ベクトル（数値型リスト）の長さを求める関数
+function Norm(number_list: number[]) {
+  return Math.sqrt(number_list[0] * number_list[0] + number_list[1] * number_list[1]);
 }
 
 export const MainContent = () => {
@@ -72,7 +94,7 @@ export const MainContent = () => {
   // me.subscribe(publication.id) の戻り値に含まれる stream
   // (contentType === "data" のもの)
   const [ otherUserDataStream, setOtherUserDataStream ] = useState<RemoteDataStream>();
-  console.log(otherUserDataStream);
+  // console.log(otherUserDataStream);
 
   // tokenとvideo要素の参照ができたら実行
   useEffect(() => {
@@ -92,7 +114,7 @@ export const MainContent = () => {
     };
 
     initialize();
-    console.log("初期化がされました！");
+    // console.log("初期化がされました！");
   }, [token, localVideo]);
 
   // 自分自身のウィンドウの位置・大きさの調整
@@ -107,12 +129,103 @@ export const MainContent = () => {
 
   // myWindowPositionが更新された時の処理
   useEffect(() => {
-    console.log("自分のデータ送信中...");
+    // console.log("自分のデータ送信中...");
     if (localDataStream != null) {
       localDataStream.write(myWindowInfo);
-      console.log("自分のデータを送信しました！");
+      // console.log("自分のデータを送信しました！");
     }
   }, [ myWindowInfo ]);
+
+  // MediaPipeを用いて，対話相手の頭部方向を取得
+  const webcamRef = useRef<Webcam>(null);
+  const resultsRef = useRef<Results>();
+
+  /** 検出結果（フレーム毎に呼び出される） */
+  const onResults = useCallback((results: Results) => {
+    // 検出結果の格納
+    resultsRef.current = results;
+
+    // 頭部方向の取得
+    let landmarks_pos_x = []  // 468個の点のx座標を格納するリスト
+    let landmarks_pos_y = []  // 468個の点のy座標を格納するリスト
+    let face_center_default_pos = []  // 正面を向いた時の顔の中心点（ここでは，飯塚さんの修論に倣って，鼻の先の座標としている．）
+    if (results.multiFaceLandmarks && results.multiFaceLandmarks[0]) {
+      for (let id = 0; id < results.multiFaceLandmarks[0].length; id++) {
+        // 特定の顔の点を取得（x座標）
+        if (results.multiFaceLandmarks[0][id].x < 0) landmarks_pos_x.push(0);
+        else if (results.multiFaceLandmarks[0][id].x > 1) landmarks_pos_x.push(1);
+        else landmarks_pos_x.push(results.multiFaceLandmarks[0][id].x);
+
+        // 特定の顔の点を取得（y座標）
+        if (results.multiFaceLandmarks[0][id].y < 0) landmarks_pos_y.push(0);
+        else if (results.multiFaceLandmarks[0][id].y > 1) landmarks_pos_y.push(1);
+        else landmarks_pos_y.push(results.multiFaceLandmarks[0][id].y);
+
+        // 正面を向いた時の顔の中心点を取得（x，y座標）
+        if (id === 1) {
+          // x座標
+          if (results.multiFaceLandmarks[0][id].x < 0) face_center_default_pos.push(0);
+          else if (results.multiFaceLandmarks[0][id].x > 1) face_center_default_pos.push(1);
+          else face_center_default_pos.push(results.multiFaceLandmarks[0][id].x);
+
+          // y座標
+          if (results.multiFaceLandmarks[0][id].y < 0) face_center_default_pos.push(0);
+          else if (results.multiFaceLandmarks[0][id].y > 1) face_center_default_pos.push(1);
+          else face_center_default_pos.push(results.multiFaceLandmarks[0][id].y);
+        }
+      }
+    }
+    // 顔の中心点の座標
+    const face_center_pos = [Average(landmarks_pos_x), Average(landmarks_pos_y)];
+    // 頭部方向を計算するためのベクトル
+    const base_vector = [1,0];  
+    // 顔の中心点を原点とした時の，正面を向いた際の顔の中心点の座標
+    const fc_d_from_fc_vector = [face_center_default_pos[0] - face_center_pos[0], face_center_default_pos[1] - face_center_pos[1]];
+    // console.log("face_center_pos = " + face_center_default_pos);
+    // console.log("face_center_default_pos = " + face_center_default_pos);
+    // console.log("fc_d_from_fc_vector = " + fc_d_from_fc_vector);
+
+    // 頭部方向（ラジアン）
+    let rad_head_direction = Math.acos(Inner(base_vector, fc_d_from_fc_vector) / (Norm(base_vector) * Norm(fc_d_from_fc_vector)));
+    // 頭部方向（度）
+    let theta_head_direction = rad_head_direction * (180 / Math.PI);
+    // arccosの値域が0～πであるため，上下の区別をするために，上を向いている時には，ラジアンおよび度の値を更新する
+    if (fc_d_from_fc_vector[1] < 0) {
+      rad_head_direction = -rad_head_direction;
+      theta_head_direction = Math.PI * 2 - theta_head_direction;
+    }
+    console.log("theta_head_direction = " + theta_head_direction);
+  },[]);
+
+  useEffect(() => {
+    const faceMesh = new FaceMesh({
+      locateFile: file => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
+      }
+    });
+
+    faceMesh.setOptions({
+      maxNumFaces: 1,
+      refineLandmarks: true, // landmarks 468 -> 478
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5
+    });
+
+    faceMesh.onResults(onResults);
+
+    if (webcamRef.current) {
+      const camera = new Camera(webcamRef.current.video!, {
+        onFrame: async () => {
+          await faceMesh.send({ image: webcamRef.current!.video! })
+        }
+      });
+      camera.start();
+    }
+
+    return () => {
+        faceMesh.close();
+    }
+  }, [onResults]);
 
   // これを field-area の div の onKeyDown に指定
   const onKeyDown = useCallback((e:React.KeyboardEvent<HTMLDivElement>) => {
@@ -160,12 +273,12 @@ export const MainContent = () => {
   }), [ otherUserWindowInfo ]);
 
   useEffect(() => {
-    console.log("相手のデータ受信設定");
+    // console.log("相手のデータ受信設定");
     if (otherUserDataStream != null) {
       // callbackで受信座標を反映する
       otherUserDataStream.onData.add((args) => {
         setOtherUserWindowInfo(args as WindowInfo);
-        console.log("相手のデータを受信しました！");
+        // console.log("相手のデータを受信しました！");
       });
     }
   }, [ otherUserDataStream ]);
@@ -255,6 +368,7 @@ export const MainContent = () => {
       <div className="field-area" tabIndex={-1} onKeyDown={ onKeyDown }>
         <div className="icon-container">
           <video id="local-video" ref={localVideo} muted playsInline style={myWindowContainerStyle}></video>
+          <Webcam id="local-video" ref={webcamRef} muted playsInline style={myWindowContainerStyle} disabled={true}/>
         </div>
         <div className="icon-container">
         {
