cb36336fae17a2a5c7e44f51c759d3be1ddeb9c7
Ishibashi Ryo <ishibashi@iplab.cs.tsukuba.ac.jp>
2024-09-16 20:11:28 +0900

    ビデオウィンドウの位置・大きさを対話相手側のスクリーン基準で更新できるようにした

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -10,6 +10,10 @@ interface WindowInfo {
     top: number;
     left: number;
     width: number;
+    screenWidth: number;
+    screenHeight: number;
+    scrollX: number;
+    scrollY: number;
 }
 
 // 数値型リストの要素の平均値を求める関数
@@ -44,38 +48,194 @@ function Norm(number_list: number[]) {
 // 移動平均計算時のフレーム数
 const MovingAverage_frame = 20;
 // 移動平均計算用の配列
-let move_top_positions: number[] = [];
-let move_left_positions: number[] = [];
-let move_width: number[] = [];
+let move_top_positions: number[][] = [[],[]];
+let move_left_positions: number[][] = [[],[]];
+let move_width: number[][] = [[],[]];
 
 // 条件ID（1: Baseline, 2: PositionChange, 3: SizeChange, 4: PositionAndSizeChange）
 let condition_ID = 1;
 // 条件名
 let condition_name = "Baseline";
 
-export const MainContent = () => {
-  // スクリーンの幅・高さ
-  // TODO: 値を要修正
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
-  // スクロール位置を取得
-  const scrollX = window.scrollX;
-  const scrollY = window.scrollY;
-  // ビデオウィンドウのデフォルトの中心位置
-  const default_center_X = scrollX + screenWidth/2;
-  const default_center_Y = scrollY + screenHeight/2;
-  // ビデオウィンドウの大きさのデフォルト値
-  const default_width = 300;
-  // ビデオウィンドウのデフォルトのtop・left位置
+// ビデオウィンドウの大きさのデフォルト値（参加者・対話相手共通）
+const default_width = 300;
+
+// スクリーンの幅・高さ（参加者側）
+let screenMyWidth = window.innerWidth;
+let screenMyHeight = window.innerHeight;
+// スクロール位置を取得（参加者側）
+let scrollMyX = window.scrollX;
+let scrollMyY = window.scrollY;
+// // ビデオウィンドウのデフォルトの中心位置（参加者側）
+// let default_Mycenter_X = scrollMyX + screenMyWidth/2;
+// let default_Mycenter_Y = scrollMyY + screenMyHeight/2;
+// // ビデオウィンドウのデフォルトのtop・left位置（参加者側）
+// let default_Mytop = default_Mycenter_Y - default_width/2;
+// let default_Myleft = default_Mycenter_X - default_width/2;
+
+// スクリーンの幅・高さ（対話相手側）
+let screenOtherWidth = window.innerWidth;
+let screenOtherHeight = window.innerHeight;
+// スクロール位置を取得（対話相手側）
+let scrollOtherX = window.scrollX;
+let scrollOtherY = window.scrollY;
+// // ビデオウィンドウのデフォルトの中心位置（対話相手側）
+// let default_Othercenter_X = scrollOtherX + screenOtherWidth/2;
+// let default_Othercenter_Y = scrollOtherY + screenOtherHeight/2;
+// // ビデオウィンドウのデフォルトのtop・left位置（対話相手側）
+// let default_Othertop = default_Othercenter_Y - default_width/2;
+// let default_Otherleft = default_Othercenter_X - default_width/2;
+
+// ビデオウィンドウの大きさの最小値・最大値
+const width_min = 50;
+const width_max = 500;
+// 移動量の拡大率
+const distance_rate_move = 10000;
+
+// 対話相手のスクリーンの幅・高さの初期設定
+function InitOtherScreenInfo(screen_width: number, screen_height: number, scroll_X: number, scroll_Y: number) {
+  screenOtherWidth = screen_width;
+  screenOtherHeight = screen_height;
+
+  scrollOtherX = scroll_X;
+  scrollOtherY = scroll_Y;
+
+  // default_Othercenter_X = scrollOtherX + screenOtherWidth/2;
+  // default_Othercenter_Y = scrollOtherY + screenOtherHeight/2;
+
+  // default_Othertop = default_Othercenter_Y - default_width/2;
+  // default_Otherleft = default_Othercenter_X - default_width/2;
+}
+
+// ビデオウィンドウのInfoの更新（index = 0：参加者自身のビデオウィンドウ，index = 1：対話相手のビデオウィンドウ）
+function setWindowInfo(fc_d_from_fc_vector: number[], rad_head_direction: number, screen_Width: number, screen_Height: number, scroll_X: number, scroll_Y: number, index: number) {
+  // ビデオウィンドウのデフォルトの中心位置（対話相手側）
+  const default_center_X = scroll_X + screenOtherWidth/2;
+  const default_center_Y = scroll_Y + screenOtherHeight/2;
+  // ビデオウィンドウのデフォルトのtop・left位置（対話相手側）
   const default_top = default_center_Y - default_width/2;
   const default_left = default_center_X - default_width/2;
 
-  // ビデオウィンドウの大きさの最小値・最大値
-  const width_min = 50;
-  const width_max = 500;
-  // 移動量の拡大率
-  const distance_rate_move = 10000;
+  // ウィンドウの大きさの最大値に対する，実際のウィンドウの大きさの比率
+  let next_width_rate = 0;
+  // 顔の中心点を原点とした時の，正面を向いた際の顔の中心点のベクトルの長さによって，ウィンドウの大きさを変更
+  if (150 * Norm(fc_d_from_fc_vector) <= 1) {
+    next_width_rate = 1;
+  }
+  else {
+    next_width_rate = 1 / (150 * Norm(fc_d_from_fc_vector));
+  }
 
+  // ウィンドウの大きさを踏まえて，ウィンドウの位置を決めるため，ウィンドウの大きさ → ウィンドウの位置の順に算出する
+  // 1. ウィンドウの大きさの算出
+  let width_value = width_max * (next_width_rate);
+
+  // 移動平均を導入するために，値を保存
+  move_width[index].push(width_value);
+
+  if (move_width[index].length < 10) width_value = Average_value(move_width[index], 0, move_width[index].length - 1);
+  else{
+    if (move_width[index].length > MovingAverage_frame + 10) move_width[index].shift();
+    width_value = Average_value(move_width[index], move_width[index].length - MovingAverage_frame, move_width[index].length - 1);
+  }
+
+  if (width_value < width_min) width_value = width_min;
+
+  // PositionChange条件の時には，top・leftの値にwidth_valueの値が影響を与えないようにするために，width_valueの値を更新
+  if (condition_ID === 2) width_value = default_width;
+
+  // 2. ウィンドウの位置の算出
+  let top_value = default_center_Y + distance_rate_move * Norm(fc_d_from_fc_vector) * Math.sin(rad_head_direction) - width_value/2;
+  let left_value = default_center_X + distance_rate_move * Norm(fc_d_from_fc_vector) * Math.cos(rad_head_direction - Math.PI) - width_value/2;
+
+  // フィールド領域をはみ出ないように調整を入れる
+  if (top_value < 0) top_value = 0;
+  else if (top_value > screen_Height - width_value) top_value = screen_Height - width_value;
+
+  if (left_value < 0) left_value = 0;
+  else if (left_value > screen_Width - width_value) left_value = screen_Width - width_value;
+
+  // 移動平均を導入するために，値を保存
+  move_top_positions[index].push(top_value);
+  move_left_positions[index].push(left_value);
+
+  // 移動平均の計算 + リストの肥大化の防止
+  if (move_top_positions[index].length < 10) top_value = Average_value(move_top_positions[index], 0, move_top_positions[index].length - 1);
+  else{
+    if (move_top_positions[index].length > MovingAverage_frame + 10) move_top_positions[index].shift();
+    top_value = Average_value(move_top_positions[index], move_top_positions[index].length - MovingAverage_frame, move_top_positions[index].length - 1);
+  }
+
+  if (move_left_positions[index].length < 10) left_value = Average_value(move_left_positions[index], 0, move_left_positions[index].length - 1);
+  else{
+    if (move_left_positions[index].length > MovingAverage_frame + 10) move_left_positions[index].shift();
+    left_value = Average_value(move_left_positions[index], move_left_positions[index].length - MovingAverage_frame, move_left_positions[index].length - 1);
+  }
+
+  let newInfo: WindowInfo;
+
+  switch(condition_ID) {
+    case 1:  // Baseline条件
+      newInfo = {
+        top: default_top,
+        left: default_left,
+        width: default_width,
+        screenWidth: screen_Width,
+        screenHeight: screen_Height,
+        scrollX: scroll_X,
+        scrollY: scroll_Y
+      };
+      break;
+    case 2:  // PositionChange条件
+      newInfo = {
+        top: top_value,
+        left: left_value,
+        width: default_width,
+        screenWidth: screen_Width,
+        screenHeight: screen_Height,
+        scrollX: scroll_X,
+        scrollY: scroll_Y
+      };
+      break;
+    case 3:  // SizeChange条件
+      newInfo = {
+        top: default_center_Y - width_value/2,
+        left: default_center_X - width_value/2,
+        width: width_value,
+        screenWidth: screen_Width,
+        screenHeight: screen_Height,
+        scrollX: scroll_X,
+        scrollY: scroll_Y
+      };
+      break;
+    case 4:  // PositionAndChange条件
+      newInfo = {
+        top: top_value,
+        left: left_value,
+        width: width_value,
+        screenWidth: screen_Width,
+        screenHeight: screen_Height,
+        scrollX: scroll_X,
+        scrollY: scroll_Y
+      };
+      break;
+    default:  // Baseline条件
+      newInfo = {
+        top: default_top,
+        left: default_left,
+        width: default_width,
+        screenWidth: screen_Width,
+        screenHeight: screen_Height,
+        scrollX: scroll_X,
+        scrollY: scroll_Y
+      };
+      break;
+  }
+
+  return newInfo;
+}
+
+export const MainContent = () => {
   const appId = useMemo(() => process.env.REACT_APP_SKYWAY_APP_ID, []);
   const secretKey = useMemo(() => process.env.REACT_APP_SKYWAY_SECRET_KEY, []);
 
@@ -162,7 +322,10 @@ export const MainContent = () => {
   }, [token, localVideo]);
 
   // 自分自身のウィンドウの位置・大きさの調整
-  const [ myWindowInfo, setMyWindowInfo ] = useState<WindowInfo>({ top: default_top, left: default_left, width: default_width });  // （中心の値を決め打ちしていいのだろうか...？）
+  const [ myWindowInfo, setMyWindowInfo ] = useState<WindowInfo>({ 
+    top: scrollMyY + screenMyWidth/2 - default_width/2, left: scrollMyX + screenMyWidth/2 - default_width/2, width: default_width, 
+    screenWidth: screenMyWidth, screenHeight: screenMyHeight, scrollX: scrollMyX, scrollY: scrollMyY
+  });
 
   const myWindowContainerStyle = useMemo<React.CSSProperties>(() => ({
       position: "absolute",
@@ -171,14 +334,20 @@ export const MainContent = () => {
       width: myWindowInfo.width
   }), [ myWindowInfo ]);
 
+  // 対話相手側に送信するウィンドウ情報の設定（対話相手側のスクリーンに基づいて，自分自身の）
+  const [ myWindowInfo_based_on_OtherScreen, setMyWindowInfo_based_on_OtherScreen ] = useState<WindowInfo>({ 
+    top: scrollOtherY + screenOtherWidth/2 - default_width/2, left: scrollOtherX + screenOtherWidth/2 - default_width/2, width: default_width, 
+    screenWidth: screenOtherWidth, screenHeight: screenOtherHeight, scrollX: scrollOtherX, scrollY: scrollOtherY
+  });
+
   // myWindowPositionが更新された時の処理
   useEffect(() => {
     // console.log("自分のデータ送信中...");
     if (localDataStream != null) {
-      localDataStream.write(myWindowInfo);
+      localDataStream.write(myWindowInfo_based_on_OtherScreen);
       // console.log("自分のデータを送信しました！");
     }
-  }, [ myWindowInfo ]);
+  }, [ myWindowInfo_based_on_OtherScreen ]);
 
   // MediaPipeを用いて，対話相手の頭部方向を取得
   const webcamRef = useRef<Webcam>(null);
@@ -242,7 +411,7 @@ export const MainContent = () => {
     // console.log("diff_top = " + distance_rate_move * Norm(fc_d_from_fc_vector) * Math.sin(rad_head_direction));
     // console.log("diff_left = " + distance_rate_move * Norm(fc_d_from_fc_vector) * Math.cos(rad_head_direction));
 
-    // 対話相手のウィンドウの大きさの変更
+    // ウィンドウの大きさの最大値に対する，実際のウィンドウの大きさの比率
     let next_width_rate = 0;
     // 顔の中心点を原点とした時の，正面を向いた際の顔の中心点のベクトルの長さによって，ウィンドウの大きさを変更
     if (150 * Norm(fc_d_from_fc_vector) <= 1) {
@@ -252,98 +421,11 @@ export const MainContent = () => {
       next_width_rate = 1 / (150 * Norm(fc_d_from_fc_vector));
     }
 
-    // TODO: 取得した頭部方向の値を基に，対話相手のウィンドウの位置・大きさを変更
     // widthの範囲：50~500？
     // 要検討：ウィンドウの動きとユーザの実際の動きを合わせるために，左右反転させる？
-    setMyWindowInfo(pre => {
-      // ウィンドウの大きさを踏まえて，ウィンドウの位置を決めるため，ウィンドウの大きさ → ウィンドウの位置の順に算出する
-      // 1. ウィンドウの大きさの算出
-      let width_value = width_max * (next_width_rate);
-
-      // 移動平均を導入するために，値を保存
-      move_width.push(width_value);
-
-      if (move_width.length < 10) width_value = Average_value(move_width, 0, move_width.length - 1);
-      else{
-        if (move_width.length > MovingAverage_frame + 10) move_width.shift();
-        width_value = Average_value(move_width, move_width.length - MovingAverage_frame, move_width.length - 1);
-      }
-
-      if (width_value < width_min) width_value = width_min;
-
-      // PositionChange条件の時には，top・leftの値にwidth_valueの値が影響を与えないようにするために，width_valueの値を更新
-      if (condition_ID === 2) width_value = default_width;
-
-      // 2. ウィンドウの位置の算出
-      let top_value = default_center_Y + distance_rate_move * Norm(fc_d_from_fc_vector) * Math.sin(rad_head_direction) - width_value/2;
-      let left_value = default_center_X + distance_rate_move * Norm(fc_d_from_fc_vector) * Math.cos(rad_head_direction - Math.PI) - width_value/2;
-
-      // フィールド領域をはみ出ないように調整を入れる
-      if (top_value < 0) top_value = 0;
-      else if (top_value > screenHeight - width_value) top_value = screenHeight - width_value;
-
-      if (left_value < 0) left_value = 0;
-      else if (left_value > screenWidth - width_value) left_value = screenWidth - width_value;
-
-      // 移動平均を導入するために，値を保存
-      move_top_positions.push(top_value);
-      move_left_positions.push(left_value);
-
-      // 移動平均の計算 + リストの肥大化の防止
-      if (move_top_positions.length < 10) top_value = Average_value(move_top_positions, 0, move_top_positions.length - 1);
-      else{
-        if (move_top_positions.length > MovingAverage_frame + 10) move_top_positions.shift();
-        top_value = Average_value(move_top_positions, move_top_positions.length - MovingAverage_frame, move_top_positions.length - 1);
-      }
-
-      if (move_left_positions.length < 10) left_value = Average_value(move_left_positions, 0, move_left_positions.length - 1);
-      else{
-        if (move_left_positions.length > MovingAverage_frame + 10) move_left_positions.shift();
-        left_value = Average_value(move_left_positions, move_left_positions.length - MovingAverage_frame, move_left_positions.length - 1);
-      }
-
-      let newInfo: WindowInfo;
-
-      switch(condition_ID) {
-        case 1:  // Baseline条件
-          newInfo = {
-            top: default_top,
-            left: default_left,
-            width: default_width
-          };
-          break;
-        case 2:  // PositionChange条件
-          newInfo = {
-            top: top_value,
-            left: left_value,
-            width: default_width
-          };
-          break;
-        case 3:  // SizeChange条件
-          newInfo = {
-            top: default_center_Y - width_value/2,
-            left: default_center_X - width_value/2,
-            width: width_value
-          };
-          break;
-        case 4:  // PositionAndChange条件
-          newInfo = {
-            top: top_value,
-            left: left_value,
-            width: width_value
-          };
-          break;
-        default:  // Baseline条件
-          newInfo = {
-            top: default_top,
-            left: default_left,
-            width: default_width
-          };
-          break;
-      }
-
-      return newInfo;
-    });
+    // 自分自身のスクリーンに対するビデオウィンドウの位置の更新（index = 0：自分自身側のスクリーン基準，index = 1：対話相手側のスクリーン基準）
+    setMyWindowInfo(pre => setWindowInfo(fc_d_from_fc_vector, rad_head_direction, screenMyWidth, screenMyHeight, scrollMyX, scrollMyY, 0));
+    setMyWindowInfo_based_on_OtherScreen(pre => setWindowInfo(fc_d_from_fc_vector, rad_head_direction, screenOtherWidth, screenOtherHeight, scrollOtherX, scrollOtherY, 1));
   },[]);
 
   useEffect(() => {
@@ -411,7 +493,10 @@ export const MainContent = () => {
 
   // 他ユーザの座標情報を保持
   // （これを自分のアイコンと同様に画面表示用のstyleに反映する）
-  const [ otherUserWindowInfo, setOtherUserWindowInfo ] = useState<WindowInfo>({ top: default_top, left: default_left, width: default_width });
+  const [ otherUserWindowInfo, setOtherUserWindowInfo ] = useState<WindowInfo>({
+    top: scrollOtherY + screenOtherWidth/2 - default_width/2, left: scrollOtherX + screenOtherWidth/2 - default_width/2, width: default_width, 
+    screenWidth: screenOtherWidth, screenHeight: screenOtherHeight, scrollX: scrollOtherX, scrollY: scrollOtherY
+   });
 
   // 他ユーザのウィンドウの位置・大きさの変更
   const otherUserWindowContainerStyle = useMemo<React.CSSProperties>(() => ({
@@ -427,6 +512,8 @@ export const MainContent = () => {
       // callbackで受信座標を反映する
       otherUserDataStream.onData.add((args) => {
         setOtherUserWindowInfo(args as WindowInfo);
+        // 対話相手のスクリーン情報の初期化
+        InitOtherScreenInfo(otherUserWindowInfo.screenWidth, otherUserWindowInfo.screenHeight, otherUserWindowInfo.scrollX, otherUserWindowInfo.scrollY);
         // console.log("相手のデータを受信しました！");
       });
     }
